5900131fa7f3d36b078ebb0cfae59606d2ea7c4c
Natsu No Color <alberta_hunterjp@yahoo.co.jp>
2017-12-18 00:51:01 +0000

    completed userfollow_unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,8 +22,7 @@ class User < ActiveRecord::Base
   has_many :reverse_relationships, foreign_key: "followed_id",
                                    class_name: "Relationship",
                                    dependent:  :destroy
-                                  
-
+  has_many :followers, through: :reverse_relationships, source: :follower                                 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
